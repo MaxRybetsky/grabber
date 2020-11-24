661e246e887e3116d90f5cf19c4ac39759a5ff0c
MaxRybetsky <stsdewar@ya.ru>
2020-11-24 21:43:42 +0300

    2.2. Модель данных - Post.

---
+++ src/main/java/ru/job4j/data/Post.java
@@ -0,0 +1,27 @@
+package ru.job4j.data;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private final String title;
+    private final String description;
+    private final LocalDateTime date;
+
+    public Post(String title, String description, LocalDateTime date, String author, String link) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+}
