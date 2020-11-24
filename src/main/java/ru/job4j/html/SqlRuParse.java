fd4655f340231d39cdb4118ba64770f921cde65d
MaxRybetsky <stsdewar@ya.ru>
2020-11-24 12:32:57 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -10,16 +10,26 @@ import java.io.IOException;
 
 public class SqlRuParse {
     public static void main(String[] args) throws IOException {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-        Elements dates = doc.select(".altCol");
-        int index = 1;
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            System.out.println(Converter.stringToLocalDateTime(dates.get(index).text()));
-            index += 2;
+        printDataFromSite(5);
+    }
+
+    public static void printDataFromSite(int repeats) throws IOException {
+        String url = "https://www.sql.ru/forum/job-offers/";
+        for (int i = 1; i <= repeats; i++) {
+            Document doc = Jsoup.connect(url + i)
+                    .get();
+            Elements row = doc.select(".postslisttopic");
+            Elements dates = doc.select(".altCol");
+            int index = 1;
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                System.out.println(href.text());
+                System.out.println(Converter.stringToLocalDateTime(
+                        dates.get(index).text()
+                ));
+                index += 2;
+            }
         }
     }
 }
